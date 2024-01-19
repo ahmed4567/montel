01e904c7d6e72beba94192c113ad7071e8300eb3
ahmed4567 <ahmed.ashref.1197@gmail.com>
2024-01-19 20:50:10 +0200

    1001

--- components/Foter.js
+++ components/Foter.js
@@ -1,34 +1,41 @@
 import React from "react";
 function Foter() {
   return (
-    <div className=" grid grid-cols-2  max-w-6xl pt-2 text-center py-2 px-5 h-fit md:grid-cols-4 m-auto ">
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">ABOUT</h5>
-
+    <div className=" grid grid-cols-2  max-w-6xl pt-2  text-center py-2 px-5 h-fit md:grid-cols-4 m-auto ">
+      <div className="space-y-3 text-xs p-1 text-gray-800">
+        <h5 className="font-bold border-b-2 border-black  transition transform duration-200">
+          ABOUT
+        </h5>
         <p>How Montil works</p>
         <p>Newsroom</p>
         <p>Investors</p>
         <p>Montil Plus</p>
         <p>Montil Luxe</p>
       </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">COMUNITY</h5>
+      <div className="space-y-3 text-xs p-1 text-gray-800">
+        <h5 className="font-bold border-b-2 border-black  transition transform duration-200">
+          COMUNITY
+        </h5>
         <p>Accessibility</p>
         <p>This is not a real site</p>
         <p>its awesome clone</p>
         <p>Referrals accepted</p>
         <p>Papafam</p>{" "}
       </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">HOST</h5>
+      <div className="space-y-3 text-xs p-1 text-gray-800">
+        <h5 className="font-bold border-b-2 border-black  transition transform duration-200">
+          HOST
+        </h5>
 
         <p>Ahmed Ashref SE</p>
         <p>presents</p>
         <p>Full Stake Web Apps</p>
         <p>Coantact now</p>
       </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">SUPORT</h5>
+      <div className="space-y-3 text-xs p-1 text-gray-800">
+        <h5 className="font-bold border-b-2 border-black  transition transform duration-200">
+          SUPORT
+        </h5>
 
         <p>Help Center</p>
         <p>Trust & safety</p>
